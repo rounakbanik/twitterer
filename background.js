e1848fe17c9278298651f841e5b82eecfcefc3c3
Rounak Banik <rounakbanik@gmail.com>
2016-08-14 22:48:14 +0530

    Tweet from a new window.

--- background.js
+++ background.js
@@ -1,9 +1,37 @@
-chrome.contextMenus.create({
-  title: "Twitterer",
-  contexts: ["selection"],
-  onclick: myFunction
-});
+contextList = ["selection", "link", "image", "page"];
 
-function myFunction(selectedText) {
-  chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+ selectedText.selectionText});
+for(i=0; i<contextList.length; i++) {
+  context = contextList[i];
+
+  chrome.contextMenus.create({
+    title: "Share this " +context+ " on Twitter",
+    contexts: [context],
+    onclick: myFunction,
+    id: context
+  });
+}
+
+
+function myFunction(data, tab) {
+  switch(data.menuItemId) {
+    case 'selection':
+      chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+
+                            encodeURIComponent(data.selectionText), type: "panel", width: 500,
+                            height: 300});
+      break;
+    case 'link':
+      chrome.windows.create({url: "https://twitter.com/intent/tweet?url="+ encodeURIComponent(data.linkUrl),
+                             type: "panel", width: 500, height: 300});
+      break;
+    case 'image':
+      chrome.windows.create({url: "https://twitter.com/intent/tweet?url="+ encodeURIComponent(data.srcUrl),
+                             type: "panel", width: 500, height: 300});
+      break;
+    case 'page':
+      chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+ encodeURIComponent(tab.title)+
+                                   "%0A&url="+encodeURIComponent(data.pageUrl),
+                            type: "panel", width: 500, height: 300});
+      break;
+  }
+
 }
